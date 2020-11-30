562ee371646e2d5d78e20e4c5038fb693c04afb9
Israa Issam Chehade <israaissam@hotmail.com>
2020-11-30 13:35:22 +0100

    Mikael

---
+++ Indkøbsapp/Interfaces/IRepositoryButik.cs
@@ -0,0 +1,17 @@
+﻿using Indkøbsapp.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Indkøbsapp.Interfaces
+{
+    interface IRepositoryButik
+    {
+        
+        public string ButiksNavn { get; set; }
+        public string Lokation { get; set; }
+        List<Butik> GetAllButikker();
+        void AddButik(Butik bk);
+    }
+}
