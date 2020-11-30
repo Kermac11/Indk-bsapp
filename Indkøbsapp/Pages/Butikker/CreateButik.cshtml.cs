007b27f1e1cb97dec97c0abebff0fc872193424e
Israa Issam Chehade <israaissam@hotmail.com>
2020-11-30 10:46:38 +0100

    Opdateringer

---
+++ Indkøbsapp/Pages/Butikker/CreateButik.cshtml.cs
@@ -0,0 +1,17 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Indkøbsapp.Interfaces;
+using Indkøbsapp.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace Indkøbsapp.Pages.Butikker
+{
+    public class CreateButikModel : PageModel
+    {
+
+    }
+}
