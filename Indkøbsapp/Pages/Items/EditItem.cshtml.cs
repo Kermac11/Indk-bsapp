98bf54d70f5f57212d649bddab0dd379e6a84bec
Sebastian Løgtholt Westergaard <seba255j@edu.easj.dk>
2020-12-03 10:30:43 +0100

    jmkbre

--- Indkøbsapp/Pages/Items/EditItem.cshtml.cs
+++ Indkøbsapp/Pages/Items/EditItem.cshtml.cs
@@ -26,6 +26,8 @@ namespace Indkøbsapp.Pages.Items
             Item=repo.GetAllButikVarer()[id];
             return Page();
         }
+        //Der skal være noget der håndterer at Id passer til item fordi den addes til en dictionary og ikke en list
+        //Den tilføjer en vare med id=0 det kan man kun gøre en gang, det er lidt mærkeligt hvis man selv skal vælge et id
         //
         //Der skal være noget til at vælge et billede måske, ellers giver det ikke mening at have det
 
