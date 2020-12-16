0b68597e07d7446cb0fe64e7a3a853edb32e1caf
Israa Issam Chehade <israaissam@hotmail.com>
2020-12-16 12:51:48 +0100

    mm

---
+++ ButikTestProject/UnitTest1.cs
@@ -0,0 +1,15 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace ButikTestProject
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            butikker = new List<Butik>();
+            
+        }
+    }
+}
