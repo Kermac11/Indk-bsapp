bf7624c03bffbe8ec381963e9772caf5d3796980
Mikael Tyge Nielsen <mika8024@edu.easj.dk>
2020-12-16 13:25:20 +0100

    Unitest added

---
+++ IndkøbsappBrugereTests/Catalog/BrugerKatalogTests.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Indkøbsapp.Catalog;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Indkøbsapp.Catalog.Tests
+{
+    [TestClass()]
+    public class BrugerKatalogTests
+    {
+        [TestMethod()]
+        public void CreateUserTest()
+        {
+            Assert.Fail();
+        }
+    }
+}
